41c6c19b37ea1deba14b64d8f2b4680937b351af
Loireamenagement <gerant@loire-amenagement.com>
2026-07-26 09:50:23 +0200

    photos_patch.js

--- photos_patch.js
+++ photos_patch.js
@@ -2,13 +2,13 @@
 // Les images vivent dans le dossier /photos/materiaux/ du repo, à côté de app.html
 if(typeof PHOTOS_DB !== "undefined") {
   PHOTOS_DB["enrobesBitumineux"] = ["photos/materiaux/enrobesBitumineux-1.jpg", "photos/realisations/enrobes-bitumineux-1.jpeg", "photos/realisations/enrobes-bitumineux-2.jpg", "photos/realisations/enrobes-bitumineux-3.jpeg"];
-  PHOTOS_DB["pavageDallage"] = ["photos/materiaux/pavageDallage-1.jpg", "photos/materiaux/pavageDallage-2.jpg", "photos/materiaux/pavageDallage-3.jpg", "photos/materiaux/pavageDallage-4.jpg", "photos/materiaux/pavageDallage-5.jpg", "photos/materiaux/pavageDallage-6.jpg", "photos/materiaux/pavageDallage-7.jpg", "photos/materiaux/pavageDallage-8.jpg", "photos/materiaux/pavageDallage-9.jpg"];
+  PHOTOS_DB["pavageDallage"] = ["photos/materiaux/pavageDallage-1.jpg", "photos/materiaux/pavageDallage-2.jpg", "photos/materiaux/pavageDallage-3.jpeg", "photos/materiaux/pavageDallage-4.jpeg", "photos/materiaux/pavageDallage-5.jpeg", "photos/materiaux/pavageDallage-6.jpeg", "photos/materiaux/pavageDallage-7.jpeg", "photos/materiaux/pavageDallage-8.jpeg", "photos/materiaux/pavageDallage-9.jpeg"];
   PHOTOS_DB["resineMarbre"] = ["photos/materiaux/resineMarbre-1.jpg"];
   PHOTOS_DB["gravierBicouche"] = ["photos/materiaux/gravierBicouche-1.jpg"];
   PHOTOS_DB["gravierStabilise"] = ["photos/materiaux/gravierStabilise-1.jpg", "photos/materiaux/gravierStabilise-2.jpg", "photos/materiaux/gravierStabilise-3.jpg"];
   PHOTOS_DB["terrassteel"] = ["photos/materiaux/terrassteel-1.jpg"];
   PHOTOS_DB["pierreNaturelle"] = ["photos/materiaux/pierreNaturelle-1.jpg"];
-  PHOTOS_DB["terrassePavage"] = ["photos/materiaux/terrassePavage-1.jpg", "photos/materiaux/terrassePavage-2.jpg", "photos/materiaux/terrassePavage-3.jpg", "photos/materiaux/terrassePavage-4.jpg", "photos/materiaux/terrassePavage-5.jpg"];
+  PHOTOS_DB["terrassePavage"] = ["photos/materiaux/terrassePavage-1.jpg", "photos/materiaux/terrassePavage-2.jpeg", "photos/materiaux/terrassePavage-3.jpeg", "photos/materiaux/terrassePavage-4.jpeg", "photos/materiaux/terrassePavage-5.jpeg"];
   PHOTOS_DB["clotureLamesBois"] = ["photos/materiaux/clotureLamesBois-1.jpg"];
   PHOTOS_DB["claireVoieBois"] = ["photos/materiaux/claireVoieBois-1.jpg"];
   PHOTOS_DB["compositeTraumgarten"] = ["photos/materiaux/compositeTraumgarten-1.jpg", "photos/materiaux/compositeTraumgarten-2.jpeg", "photos/materiaux/compositeTraumgarten-3.jpeg", "photos/materiaux/compositeTraumgarten-4.jpeg", "photos/materiaux/compositeTraumgarten-5.jpeg", "photos/materiaux/compositeTraumgarten-6.jpeg", "photos/materiaux/compositeTraumgarten-7.jpeg", "photos/materiaux/compositeTraumgarten-8.jpeg", "photos/materiaux/compositeTraumgarten-9.jpeg"];
